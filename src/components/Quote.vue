ec731b716d9a3237c3a05832383b669246c4fac5
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-08-04 10:16:27 +0300

    Created Quote component

---
+++ src/components/Quote.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-md-4">
+    <div class="card card-body">
+      <slot></slot>
+    </div>
+  </div>  
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  .card {
+    font-family: 'Sacramento', 'Roboto', Arial, Helvetica, sans-serif;
+    transition: all 150ms ease-in-out;
+    font-size: 1.75rem;
+  }
+
+  .card:hover {
+    background-color: #E3655B;
+    color: #FFFFFF;
+    cursor: pointer;
+  }
+</style>
