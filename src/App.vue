c6f851ed4a662c3efef5de7f13a0902cee28b1c2
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-08-04 10:17:33 +0300

    Display The QuoteGrid component with a single quote for testing

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="container">
-    
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue'
 
 export default {
-  
+  data() {
+    return {
+      quotes: [
+        'Design is not just what it looks like and feels like. Design is how it works.'
+      ],
+      maxQuotes: 10
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid
+  }
 }
 </script>
 
