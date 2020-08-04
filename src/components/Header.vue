04fbef29be4334d661615bba5ebbc4ed2b2393f0
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-08-04 11:38:04 +0300

    Created Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>Quotes Added</h3>
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" :style="{width: (quoteCount / maxQuotes) * 100 + '%'}" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+    </div>
+    <h5 class="mt-3">{{ quoteCount }} / {{ maxQuotes }}</h5>
+  </div>  
+</template>
+
+<script>
+export default {
+  props: ['quoteCount', 'maxQuotes']
+}
+</script>
