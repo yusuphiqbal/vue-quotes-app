425b7c4a72643e837a1260eeea843bd2ec393aef
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-08-04 11:05:10 +0300

    Added some animation when a quote is hovered on

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="col-md-4">
-    <div class="card card-body">
+    <div class="card card-body mb-4 animate__animated" :class="{animate__headShake: isHovering}" @mouseover="isHovering = true" @mouseout="isHovering = false">
       <slot></slot>
     </div>
   </div>  
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      isHovering: false
+    }
+  }
+}
 </script>
 
 <style scoped>
   .card {
     font-family: 'Sacramento', 'Roboto', Arial, Helvetica, sans-serif;
-    transition: all 150ms ease-in-out;
+    transition: all ease-in-out 150ms;
     font-size: 1.75rem;
   }
 
@@ -21,5 +27,6 @@
     background-color: #E3655B;
     color: #FFFFFF;
     cursor: pointer;
+    animation-delay: 400ms;
   }
 </style>
