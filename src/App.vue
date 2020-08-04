ff7fb836c567b1c8072f32e35f7cc7ae4792903e
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-08-04 11:06:39 +0300

    Display NewQuote component and added styled app with roboto font

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="container">
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <div class="row mb-5">
+      <div class="col">
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="alert alert-info mb-4">
+          <span>Click on a quote to delete</span>
+        </div>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import QuoteGrid from './components/QuoteGrid.vue'
+import NewQuote from './components/NewQuote'
 
 export default {
   data() {
@@ -17,11 +30,19 @@ export default {
     }
   },
   components: {
-    appQuoteGrid: QuoteGrid
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
+  },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote)
+    }
   }
 }
 </script>
 
 <style>
-
+  .container {
+    font-family: 'Roboto', Arial, Helvetica, sans-serif;
+  }
 </style>
